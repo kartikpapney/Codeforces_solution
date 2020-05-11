3d43fe7b1a96068dbfe8d11729179207f5cb9ca2
Kartik Papney <64838174+kartikpapney@users.noreply.github.com>
2020-05-11 18:54:56 +0530

    Create UltraFastMathematician.cpp

---
+++ UltraFastMathematician.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	for(int i=0; a[i]!='\0'; i++)
+	{
+		if(a[i] == b[i])
+			cout << '0';
+		else
+			cout << '1';
+		
+	}
+}
