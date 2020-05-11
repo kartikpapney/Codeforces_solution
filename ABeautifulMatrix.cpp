202d1661207a03a3eb2e4ebb83739fb0276bf5ac
kartikpapney <64838174+kartikpapney@users.noreply.github.com>
2020-05-11 16:13:33 +0530

    Create ABeautifulMatrix.cpp

---
+++ ABeautifulMatrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool x;
+	for(int i=1; i<=5; i++)
+	{
+		for(int j=1; j<=5; j++)
+		{
+			cin >> x;
+			if(x == 1)
+				cout << abs(3-i)+abs(3-j);
+		}
+	}
+}
