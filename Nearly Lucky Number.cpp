761ed89487d0bc832f7d933946f3c5997bf2bc76
Kartik Papney <64838174+kartikpapney@users.noreply.github.com>
2020-05-21 10:29:20 +0530

    Create Nearly Lucky Number.cpp

---
+++ Nearly Lucky Number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    int count = 0;
+    for(int i=0; i<str.length(); i++)
+    {
+        if(str[i]=='4' || str[i]=='7')
+        {
+            count++;
+        }
+    }
+    if(count == 4 || count == 7)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+}
